616c88ebcf2bc3493d3bc161090bac9eee3b2d30
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-04-25 12:27:18 +0300

    test

--- components/headRowElem.tsx
+++ components/headRowElem.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const HeadRowElem = ({ children }: { children: React.ReactNode }) => {
  return (
   <div
-   className={`border-r-2 border-slate-800 w-[calc(100%/25)] text-sm text-center`}
+   className={`border-r-2 border-slate-800 w-[calc(100%/${process.env.MODELS})] text-sm text-center`}
   >
    {children}
   </div>
